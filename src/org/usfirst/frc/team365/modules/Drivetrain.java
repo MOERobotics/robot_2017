ba10116c5e22ff9549ecff21fe3f9716d5ec1ab2
AdrianJ199 <jenkinsa17@sanfordschool.org>
2017-02-05 14:14:08 -0500

    The Drive motoros are updated

--- src/org/usfirst/frc/team365/modules/Drivetrain.java
+++ src/org/usfirst/frc/team365/modules/Drivetrain.java
@@ -1,15 +1,18 @@
 package org.usfirst.frc.team365.modules;
-import org.usfirst.frc.team365.math.PIDOut;
+import org.usfirst.frc.team365.robot.SharedVariables;
 import org.usfirst.frc.team365.util.RobotModule;
 
+import com.ctre.CANTalon;
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+//import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-
-<<<<<<< HEAD
-
-
-
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -21,82 +24,131 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 public class Drivetrain extends RobotModule  implements PIDOutput {
 
+	public Drivetrain(RobotInputs inputs, RobotOutputs outputs) {
+		super(inputs, outputs);
+		// TODO Auto-generated constructor stub
+	}
+
 	AHRS navX;
 	
-	CANTalon driveLA = new CANTalon(0); // 
-	CANTalon driveLB = new CANTalon(1); //
-	CANTalon driveLC = new CANTalon(2); //
+	CANTalon driveLA = new CANTalon(0); // Left 1 
+	CANTalon driveLB = new CANTalon(1); // Left 2
+	CANTalon driveLC = new CANTalon(2); // Left 3
 	//CANTalon driveLD = new CANTalon(15);
-	CANTalon driveRA = new CANTalon(13); //
-	CANTalon driveRB = new CANTalon(14); //
-	CANTalon driveRC = new CANTalon(15);  //
+	CANTalon driveRA = new CANTalon(13); // Right 1
+	CANTalon driveRB = new CANTalon(14); // Right 2
+	CANTalon driveRC = new CANTalon(15);  // Right 3
 	//CANTalon driveRD = new CANTalon(4);
 	
 	
 	
-	Joystick driveStick; //
-	Joystick funStick;
+	Joystick driveStick; // the drive stick
+	Joystick funStick; // the function stick
 	Encoder leftEncoder;
-=======
-public class Drivetrain extends RobotModule{
->>>>>>> da620aac756436d30c93ddb38c85afc6d63fcfb3
-	int autoLoopCounter;
-	int autoStep;
-	int teleopLoopCounter;
-	double direction;
+	int autoLoopCounter; // counts the loops of autonomous
+	int autoStep; 
+	int teleopLoopCounter; // 
+	double direction; //
 	
-	PIDOut driveCorrection;
+	DoubleSolenoid gearShift = new DoubleSolenoid(0,1); //
+	
+	
 	PIDController driveStraight;
 	
-    public Drivetrain(RobotInputs inputs, RobotOutputs outputs){
-    	super(inputs, outputs);
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {
+    	
+    	driveRA.setInverted(true); // 
+    	driveRB.setInverted(true); // 
+    	driveRC.setInverted(true); // 
+    	
+    	driveLA.enableBrakeMode(true); // 
+    	driveLB.enableBrakeMode(true); // 
+    	driveLC.enableBrakeMode(true); // 
+    	driveRA.enableBrakeMode(true); // 
+    	driveRB.enableBrakeMode(true); // 
+    	driveRC.enableBrakeMode(true); // 
+    	driveStick = new Joystick(0); // 
+    	funStick = new Joystick(1);
+    	
+    	gearShift.set(Value.kReverse); // 
+    	
+    	SharedVariables.registerKeyspace("DriveTrain"); // 
+    	SharedVariables.registerKey("DriveTrain","Turn"); // 
+    	
+    	driveStraight = new PIDController(0.04, 0.00005, 0.03, navX, this); // 
+    	driveStraight.setContinuous(); // 
+    	driveStraight.setInputRange(-180.0, 180.0); // 
+    	driveStraight.setOutputRange(-1.0, 1.0); // 
     }
-	
-    public void robotInit(){    	
-    	outputs.gearShift.set(Value.kReverse);
-    	driveCorrection=new PIDOut();
-    	driveStraight = new PIDController(0.04, 0.00005, 0.03, inputs.navx, driveCorrection);
-    	driveStraight.setContinuous();
-    	driveStraight.setInputRange(-180.0, 180.0);
-    	driveStraight.setOutputRange(-1.0, 1.0);
-    }
+    
+    /**
+     * This function is run once each time the robot enters autonomous mode
+     */
     public void disabledInit () {
-    	if (driveStraight.isEnabled()) {
-    		driveStraight.disable();
-    	}
+    	if (driveStraight.isEnabled()) { // 
+    		driveStraight.disable(); // 
+    	} // 
     }
+    
     public void disabledPeriodic () {
-    	if(inputs.driveStick.getTrigger()) {
-    		//leftEncoder.reset();
-    	}
+    	if(driveStick.getTrigger()) { // 
+    		leftEncoder.reset(); // 
+    	} // 
     }
+    
+    
+    
+    
+    
+    
+    
     public void autonomousInit() {
     	autoLoopCounter = 0;
     	autoStep = 1;
     }
+
     public void autonomousPeriodic() {
     	
     }
-
+   
     
+    
+    
+    
+    
+    
+    /**
+     * This function is called once each time the robot enters tele-operated mode
+     */
     public void teleopInit(){
-    	teleopLoopCounter = 0;
-    	outputs.gearShift.set(Value.kForward);
+    	teleopLoopCounter = 0; // 
+    	gearShift.set(Value.kForward); // 
+    	
     }
+
+    /**
+     * This function is called periodically during operator control
+     */
     public void teleopPeriodic() {
-    	teleopLoopCounter ++;
-        double xJoy = inputs.driveStick.getX();
-        double yJoy = -inputs.driveStick.getY();
+    	teleopLoopCounter ++; // 
+        double xJoy = driveStick.getX(); // 
+        double yJoy = -driveStick.getY(); // 
       
-        double leftMotor;
-        double rightMotor;
-        if (inputs.driveStick.getRawButton(6)) {
-        	outputs.gearShift.set(Value.kReverse);
-        } else {
-        	outputs.gearShift.set(Value.kForward);
-        }
+        double leftMotor; // 
+        double rightMotor; // 
+        if (driveStick.getRawButton(6))  // 
+        { // 
+        	gearShift.set(Value.kReverse); // 
+        } // 
+        else // 
+        { // 
+        	gearShift.set(Value.kForward); // 
+        } // 
         
-<<<<<<< HEAD
         if (driveStick.getTrigger()) { // 
         	rightMotor = yJoy; // 
         	leftMotor = yJoy; // 
@@ -118,42 +170,41 @@ public class Drivetrain extends RobotModule{
         {
         	leftMotor *= .5;
         	rightMotor *= .5
-=======
-        if (inputs.driveStick.getTrigger()) {
-        	rightMotor = yJoy;
-        	leftMotor = yJoy;
-        } else {
-        	rightMotor = limitMotor(yJoy - xJoy);
-        	leftMotor = limitMotor(yJoy + xJoy);
->>>>>>> da620aac756436d30c93ddb38c85afc6d63fcfb3
         }
-        if (inputs.driveStick.getRawButton(8)){
-        	leftMotor = 0.5 * leftMotor;
-            rightMotor = 0.5 * rightMotor;
-        }
-        driveRobot(leftMotor, rightMotor);
+         */
+       
     }
+    
+    
+    /**
+     * This function is called periodically during test mode
+     */
+    
+    
     public void testInit() {
     	
     }
+    
     public void testPeriodic() {
     	LiveWindow.run();
     }
-    public void pidDrive() {
-    	double output = driveCorrection.getOutput();
-    	double right = direction - output;
-    	double left = direction + output;
-    	driveRobot(left, right);
-    }
+    
+    public void pidWrite(double output) { // 
+    	double right = direction - output; // 
+    	double left = direction + output; // 
+    	driveRobot(left, right); // 
+    } // 
     
     public void driveRobot(double leftMotor, double rightMotor){
-        outputs.driveLF.set(leftMotor);
-        outputs.driveLM.set(leftMotor);
-        outputs.driveLR.set(leftMotor);
-        outputs.driveRF.set(rightMotor);
-        outputs.driveRM.set(rightMotor);
-        outputs.driveRR.set(rightMotor);
-    }
+        driveLA.set(leftMotor);
+        driveLB.set(leftMotor);
+        driveLC.set(leftMotor);
+       // driveLD.set(leftMotor);
+        driveRA.set(rightMotor);
+        driveRB.set(rightMotor);
+        driveRC.set(rightMotor);
+        //driveRD.set(rightMotor); 
+    	}
    
     double limitMotor(double motorLimit) { // 
     	if (motorLimit > 1) return 1; // 
@@ -164,6 +215,7 @@ public class Drivetrain extends RobotModule{
 	@Override
 	public void robotPeriodic()
 	{
+		// TODO Auto-generated method stub
 		
 	}
 }
