b31565caca6b4932c11d6f9379479d2aecc6eef5
Kevin West <kevok93@gmail.com>
2017-02-23 20:30:36 -0500

    Threaded encoder Tests for one/two cameras, and flashy-nonflashy lights

--- vision/src/Testing.java
+++ vision/src/Testing.java
@@ -1,36 +1,27 @@
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.nio.IntBuffer;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Future;
-
-import org.bytedeco.javacpp.*;
-import org.bytedeco.javacpp.opencv_videoio.VideoCapture;
-
-import javax.swing.WindowConstants;
-
-import com.pi4j.io.gpio.*;
-import com.pi4j.io.gpio.event.GpioPinListener;
-import com.pi4j.io.gpio.impl.GpioPinImpl;
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.RaspiPin;
 import com.pi4j.system.SystemInfo;
+import org.bytedeco.javacpp.BytePointer;
+import org.bytedeco.javacpp.IntPointer;
+import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacpp.opencv_imgcodecs;
 import org.bytedeco.javacv.*;
 import org.bytedeco.javacv.FrameGrabber.Exception;
-import static org.bytedeco.javacpp.avcodec.*;
-import static org.bytedeco.javacpp.avformat.*;
-import static org.bytedeco.javacpp.avutil.*;
-import static org.bytedeco.javacpp.opencv_imgproc.CV_BGR2RGB;
-import static org.bytedeco.javacpp.swscale.*;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.bytedeco.javacpp.avcodec.avpicture_get_size;
+import static org.bytedeco.javacpp.avutil.AV_PIX_FMT_RGB24;
 
 public class Testing {
-	static Scanner input = new Scanner(System.in);
-	static String userChoice;
     static OpenCVFrameConverter.ToMat FrameToMatConverter = new OpenCVFrameConverter.ToMat();
-	public static void main(String[] args) throws java.lang.Exception {
+
+    public static void main(String[] args) throws java.lang.Exception {
         if (System.getProperty("debug") != null) {
             System.err.print("\nWaiting for first connection...\n");
             for (int i = 10; i > 0; i--) {
@@ -39,21 +30,23 @@ public class Testing {
 
             }
         }
-		try {a();}
-		catch (Exception e) {
-			e.printStackTrace();
-			System.exit(42);
-		}
-	}
-	static FrameGrabber grabber;
-	public static void a() throws java.lang.Exception{
-	    System.err.print("\nStarting execution");
+        try {a();} catch (Exception e) {
+            e.printStackTrace();
+            System.exit(42);
+        }
+    }
+
+    static FrameGrabber grabber;
+
+    public static void a() throws java.lang.Exception {
+        System.err.print("\nStarting execution");
+        String videoLocation = findVideoDevices().get(0).getAbsolutePath();
 
         System.err.print("\nOpening Camera...");
-        avcodec.avcodec_register_all();
-        avcodec.AVCodec avc = avcodec.avcodec_find_encoder(avcodec.AV_CODEC_ID_JPEG2000);
 
-        grabber = new OpenCVFrameGrabber(0);
+        grabber = new FFmpegFrameGrabber(videoLocation);
+        grabber.setImageWidth(424);
+        grabber.setImageHeight(240);
         //grabber.setFormat("video4linux2");
         //grabber.setFrameRate(1);
         //grabber.setSampleRate(1);
@@ -64,7 +57,6 @@ public class Testing {
         System.err.print("Done!");
 
         System.err.print("\nChecking arch..");
-        String arch = SystemInfo.getOsArch();
         GpioController gpio = null;
         GpioPinDigitalOutput outpin = null;
 
@@ -73,7 +65,7 @@ public class Testing {
         } else {
             System.err.print("I Think we are pi");
             gpio = GpioFactory.getInstance();
-            outpin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01);
+            outpin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00);
         }
 
         /*
@@ -91,8 +83,8 @@ public class Testing {
 
 
         System.err.print("\nStarting servers...");
-		MJPEGserver server1 = new MJPEGserver(8000);
-		server1.start();
+        MJPEGserver server1 = new MJPEGserver(8000);
+        server1.start();
         System.err.print("1,");
         MJPEGserver server2 = new MJPEGserver(8001);
         server2.start();
@@ -101,100 +93,85 @@ public class Testing {
         server3.start();
         System.err.print("and 3...");
 
+        System.err.print("\nStarting encoders...");
+        EncoderThread encoder1 = new EncoderThread();
+        encoder1.start();
 
         System.err.print("\nStarting the main loop...");
         int frameCycleIndex = 0;
-		do {
-            System.err.print("\nGrabbin...");
-			Frame frame = grabber.grab();
-            System.err.print("Printin...");
-            opencv_core.Mat mat = FrameToMatConverter.convert(frame);
-            opencv_core.Mat rgb_mat = new opencv_core.Mat();
+        long frameProfileCounter = 0;
+        long start = System.currentTimeMillis();
+        int frameProfileLength = 600;
+        do {
+            frameProfileCounter = (frameProfileCounter + 1) % 600;
+            if (frameProfileCounter == 0) {
+                long end = System.currentTimeMillis();
+                double dTime = (end - start) / 1000.0;
+                System.out.printf(
+                        "\n%d frames at %f frames per second",
+                        frameProfileLength,
+                        frameProfileLength / dTime
+                );
+                start = end;
+            }
+            //System.err.print("\nGrabbin...");
+            Frame frame = ((FFmpegFrameGrabber)grabber).grabImage();
+            //System.err.print("Printin...");
 
-            opencv_imgproc.cvtColor(mat,rgb_mat,CV_BGR2RGB);
+            opencv_core.Mat m = new opencv_core.Mat();
+            FrameToMatConverter.convert(frame).copyTo(m);
+            encoder1.setWorkItem(m);
+            //c1.showImage(frame);
+            if (encoder1.outputImage == null) continue;
+            byte[] image = encoder1.outputImage.getStringBytes();
 
-            AVPicture avp = new AVPicture();
-            avpicture_fill(avp,rgb_mat.data(),AV_PIX_FMT_RGB24,480,320);
-            SwsContext sws_cx = sws_getContext(
-                    480,320,AV_PIX_FMT_RGB24,
-                    480,320,AV_PIX_FMT_YUV420P,
-                    SWS_FAST_BILINEAR,null,null,(DoublePointer)null
-            );
+            //c1.showImage(frame);
 
-            AVFrame avf = av_frame_alloc()
-                    .format(AV_PIX_FMT_YUV420P)
-                    .width(480)
-                    .height(320);
-
-
-
-            sws_scale(
-                    sws_cx,
-                    avp.data(),avp.linesize(),
-                    0,420,
-                    avf.data(),avf.linesize()
-            );
-
-            avcodec.AVCodecContext avc_cx =
-                avcodec.avcodec_alloc_context3(avc)
-                    .bit_rate(400000)
-                    .width(480)
-                    .height(320)
-                    .time_base(avutil.av_make_q(1,25))
-                    .pix_fmt(AV_PIX_FMT_YUV420P);
-            avcodec.avcodec_open2(avc_cx,avc,(PointerPointer)null);
-
-            AVPacket avpk = new AVPacket()
-                .size(0)
-                .data(null);
-            av_init_packet(avpk);
-
-            avcodec_encode_video2(avc_cx,avpk,avf,new int[0]);
-
-            byte[] img = avp.asByteBuffer().array();
-
-			//c1.showImage(frame);
-
-            System.err.print("Streamin...");
-            server1.streamData(img);
+            //System.err.print("Streamin...");
+            server1.streamData(image);
 
 
             switch (frameCycleIndex++) {
                 case 0:
                     if (outpin != null) outpin.low();
                     break;
-                case 1:
-                    System.err.print("Doin it again!");
+
+                    case 1:
+                    //System.err.print("Doin it again!");
                     //c2.showImage(frame);
-                    server2.streamData(img);
+                    server2.streamData(image);
                     break;
                 case 2:
                     if (outpin != null) outpin.high();
                     break;
                 case 3:
-                    System.err.print("Doin it again!");
+                    //System.err.print("Doin it again!");
                     //c3.showImage(frame);
-                    server3.streamData(img);
+                    server3.streamData(image);
                     break;
             }
-            frameCycleIndex %=4;
+            frameCycleIndex %= 4;
 
-		} while ( frameCycleIndex != 99
+        } while (frameCycleIndex != 99
             //c1.isDisplayable() &&
             //c2.isDisplayable() &&
             //c3.isDisplayable()
-        );
+                );
         System.err.print("\nBRB dying\n");
-		
-		//c1.dispose();
+
+        //c1.dispose();
         //c2.dispose();
         //c3.dispose();
-		server1.stop();
+        server1.stop();
         server2.stop();
         server3.stop();
-		grabber.stop();
-	}
-	
-	
-	
+        grabber.stop();
+    }
+
+    public static List<File> findVideoDevices() {
+        File[] filelist = new File("/dev/").listFiles((dir, name) -> name.matches("^video\\d+"));
+        return Arrays.asList(filelist);
+    }
+
+
 }
