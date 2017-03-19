4d5911610be03d7d8372c770dff2ce10ded0d534
mailmindlin <mailmindlin@users.noreply.github.com>
2017-03-19 14:21:02 -0400

    Maybe fixed things
    I'm not sure anymore

--- src/org/usfirst/frc/team365/net/UDPTracker.java
+++ src/org/usfirst/frc/team365/net/UDPTracker.java
@@ -9,31 +9,43 @@ import java.util.Vector;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class UDPTracker implements MOETracker {
-	static final short noneFound = 1, oneFound = 2, twoFound = 3;
-	protected int lastID;
-	protected DatagramSocket socket;
-	protected String name;
-	protected Vector<Box> boxVector;
-
-	public UDPTracker(String name, final int port) throws SocketException {
-		this.name = name;
-		socket = new DatagramSocket(port);
-		boxVector = new Vector<Box>();
-	}
-	public static UDPTracker getTracker(String name, final int port){
-		try{
-			SmartDashboard.putString("is not a good", "no");
+	static final short NONE_FOUND = 1;
+	static final short ONE_FOUND  = 2;
+	static final short TWO_FOUND  = 3;
+	
+	public static UDPTracker getTracker(String name, final int port) {
+		try {
+			SmartDashboard.putString("is great", "yes");
 			return new UDPTracker(name, port);
-		}catch (SocketException e){
+		} catch (SocketException e) {
 			e.printStackTrace();
-			SmartDashboard.putString("is not a good", "yes: "+e.getMessage());
+			SmartDashboard.putString("is great", "no: "+e.getMessage());
 			return null;
 		}
 	}
+	
+	/**
+	 * Name of this tracker
+	 */
+	protected final String name;
+	/**
+	 * Socket that this tracker listens on
+	 */
+	protected final DatagramSocket socket;
+	protected final List<Box> boxes = new ArrayList<>(2);
+
+	public UDPTracker(String name, final int port) throws SocketException {
+		this.name = name;
+		this.socket = new DatagramSocket(port);
+		
+	}
+	
+	@Override
 	public void run() {
 		ByteBuffer buf = ByteBuffer.allocate(72);
 		DatagramPacket packet = new DatagramPacket(buf.array(), buf.limit());
-		while (true) {
+		int lastId = Integer.MIN_VALUE;
+		while (!Thread.interrupted()) {
 			try {
 				buf.position(0);
 				packet.setLength(buf.limit());
@@ -46,8 +58,8 @@ public class UDPTracker implements MOETracker {
 				int boxAmnt = status - 1;
 				synchronized (this) {
 					boxVector.clear();
-					for(int n=0;n<boxAmnt;n++)
-						boxVector.add(readBox(buf));
+					for(int n = 0; n < boxAmnt; n++)
+						boxes.add(readBox(buf));
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -56,37 +68,42 @@ public class UDPTracker implements MOETracker {
 		}
 		socket.close();
 	}
-	private Box readBox(ByteBuffer buf){
+	
+	private Box readBox(ByteBuffer buf) {
 		double x = buf.getDouble();
 		double y = buf.getDouble();
 		double w = buf.getDouble();
 		double h = buf.getDouble();
-		return new Box((x + w / 2), (y + h / 2), w*h);
+		return new Box((x + w / 2), (y + h / 2), w, h);
 	}
-	public synchronized double[] getCenter(){
-		try{
+	
+	public synchronized double[] getCenter() {
+		try {
 			Box box;
 			synchronized (this) {
 				box = boxVector.get(0);
 				SmartDashboard.putNumber("raw x target", box.x);
 				SmartDashboard.putNumber("raw y target", box.y);
-				SmartDashboard.putString("is not a good", "no");
-				SmartDashboard.putString("is not a good", "n");
+				//I... give up. iIi can'T TAke it anymORE
+				SmartDashboard.putString("is great", "definitlee");
 				return new double[]{box.x, box.y};
 			}
-		}catch(Exception e){
+		} catch(Exception e) {
 			e.printStackTrace();
-			SmartDashboard.putString("is not a good", "a bit: "+e.getMessage());
+			SmartDashboard.putString("is great", "needs to be made so again: "+e.getMessage());
 			return new double[]{-1,-1};
 		}
 	}
-	private class Box{
-		@SuppressWarnings("unused")
-		double x,y,area;
-		public Box(double x, double y, double area){
-			this.x=x;
-			this.y=y;
-			this.area=area;
+	
+	private class Box {
+		final double x;
+		final double y;
+		final double area;
+		
+		public Box(double x, double y, double width, double height){
+			this.x = x;
+			this.y = y;
+			this.area = width * height;
 		}
 	}
 }
