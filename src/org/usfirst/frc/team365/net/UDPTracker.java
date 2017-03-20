188c520b6b48f49bb8a35a7dc9a4cef2032eb0f1
mailmindlin <mailmindlin@users.noreply.github.com>
2017-03-20 16:21:26 -0400

    Make UDPTracker less not correct

--- src/org/usfirst/frc/team365/net/UDPTracker.java
+++ src/org/usfirst/frc/team365/net/UDPTracker.java
@@ -4,14 +4,20 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 import java.nio.ByteBuffer;
-import java.util.Vector;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+/**
+ * Tracker that recieves coordinates via UDP packets
+ */
 public class UDPTracker implements MOETracker {
+	//packet status codes
 	static final short NONE_FOUND = 1;
 	static final short ONE_FOUND  = 2;
 	static final short TWO_FOUND  = 3;
+	static final short ERROR = (short) 0x8000;
+	static final short HELLO = (short) 0x8001;
+	static final short GOODBYE=(short) 0x8002;
 	
 	public static UDPTracker getTracker(String name, final int port) {
 		try {
@@ -32,41 +38,70 @@ public class UDPTracker implements MOETracker {
 	 * Socket that this tracker listens on
 	 */
 	protected final DatagramSocket socket;
-	protected final List<Box> boxes = new ArrayList<>(2);
+	
+	/**
+	 * Array of boxes from the most recent packet
+	 */
+	protected volatile Box[] boxes;
 
 	public UDPTracker(String name, final int port) throws SocketException {
 		this.name = name;
 		this.socket = new DatagramSocket(port);
-		
 	}
 	
 	@Override
 	public void run() {
-		ByteBuffer buf = ByteBuffer.allocate(72);
-		DatagramPacket packet = new DatagramPacket(buf.array(), buf.limit());
+		ByteBuffer buf = ByteBuffer.allocate(256);
+		DatagramPacket packet = new DatagramPacket(buf.array(), buf.capacity());
 		int lastId = Integer.MIN_VALUE;
-		while (!Thread.interrupted()) {
-			try {
-				buf.position(0);
-				packet.setLength(buf.limit());
+		
+		try {
+			while (!Thread.interrupted()) {
+				buf.clear();
+				packet.setLength(buf.capacity());
 				socket.receive(packet);
+				
 				final int id = buf.getInt();
-				if (id <= lastID)
-					continue;
-				lastID = id;
 				final short status = buf.getShort();
-				int boxAmnt = status - 1;
-				synchronized (this) {
-					boxes.clear();
-					for(int n = 0; n < boxAmnt; n++)
-						boxes.add(readBox(buf));
+				if (status == 0) {
+					//Ignore
+					continue;
+				} else if (status >= 0x8000) {
+					//Metadata packet (never stale)
+					switch (status) {
+						case HELLO:
+							//Reset id
+							lastId = id;
+							continue;
+						case ERROR:
+						case GOODBYE:
+						default:
+							//TODO handle
+							continue;
+					}
+				} else if (id <= lastID) {
+					//Stale packet
+					continue;
 				}
-			} catch (Exception e) {
-				e.printStackTrace();
-				break;
+				
+				//Update lastID
+				lastID = id;
+				
+				//Read boxes
+				int numBoxes = status - 1;
+				Box[] boxes = new Box[numBoxes];
+				for (int i = 0; i < numBoxes; i++)
+					boxes[i] = readBox(buf);
+				
+				//Update value
+				this.boxes = boxes;
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			socket.close();
 		}
-		socket.close();
 	}
 	
 	private Box readBox(ByteBuffer buf) {
@@ -77,21 +112,30 @@ public class UDPTracker implements MOETracker {
 		return new Box((x + w / 2), (y + h / 2), w, h);
 	}
 	
-	public synchronized double[] getCenter() {
+	protected Point2D doGetCenter() {
+		Box[] boxes = this.boxes;
+		if (boxes == null || boxes.length == 0) {
+			//We have no boxes
+			return null;
+		}
+		//TODO average them all or something
+		Box box = boxes[0];
+		return new Point2D.Double(box.x, box.y);
+	}
+	
+	@Override
+	public double[] getCenter() {
 		try {
-			Box box;
-			synchronized (this) {
-				box = boxes.get(0);
-			}
-			SmartDashboard.putNumber("raw x target", box.x);
-			SmartDashboard.putNumber("raw y target", box.y);
+			Point2D center = doGetCenter();
+			SmartDashboard.putNumber("raw x target", center.getX());
+			SmartDashboard.putNumber("raw y target", center.getY());
 			//I... give up. iIi can'T TAke it anymORE
 			SmartDashboard.putString("is great", "definitlee");
-			return new double[]{box.x, box.y};
+			return new double[]{center.getX(), center.getY()};
 		} catch(Exception e) {
 			e.printStackTrace();
-			SmartDashboard.putString("is great", "needs to be made so again: "+e.getMessage());
-			return new double[]{-1,-1};
+			SmartDashboard.putString("is great", "needs to be made so again: " + e.getMessage());
+			return new double[]{-1, -1};
 		}
 	}
 	
